577e8ac6d8933ed403ea938001ff5d04862f7b7d
simon <simon.hobbs@electrooptical.net>
2021-05-07 12:00:21 -0400

    updating

--- modbus_generator/ModbusEntries.py
+++ modbus_generator/ModbusEntries.py
@@ -1,14 +1,30 @@
 import os, sys, datetime, math
 from jinja2 import Template, Environment, FileSystemLoader
 from enum import Enum, IntEnum
-from data_stores import DataType, GetDataTypeCType, GetDataTypeSize
 import data_stores
+
+def GetDataTypeCType(dtype):
+    if dtype == "string":
+        return "uint8_t"
+    else:
+        return dtype + "_t"
+
+
+def GetDataTypeSize(dtype):
+    if dtype in ["uint8", "int8", "string"]:
+        return 1
+    elif dtype in ["uint16", "int16"]:
+        return 2
+    elif dtype in ["uint32", "int32"]:
+        return 4
+    if dtype in ["uint64", "int64"]:
+        return 8
 
 
 def SetAddresses(entries):
     address = 0
     for entry in entries:
-        if entry.dtype not in [DataType.uint16_t, DataType.int16_t]:
+        if entry.dtype not in ["uint16", "int16"]:
             entry.address = address + address % 2
         else:
             entry.address = address
@@ -20,30 +36,35 @@ class FunctionType(IntEnum):
     kHolding = 4
 
 
-class ModbusRegister(data_stores.StructEntry):
-    def __init__(self, name, dtype, length, function_type):
-        super().__init__(name, dtype, length)
-        self.storage_type = DataType.uint16_t
+class Entry:
+    storage_type="uint16"
+    def __init__(self, name, dtype, length, function_type, address=None):
+        self.name = name
+        self.dtype = dtype
+        self.length = length
         self.function_type = function_type
+        self._address = address
+
+        assert(type(self.name) is str)
+        assert(type(self.dtype) is str)
+        assert(self.length >= 1)
 
         assert(self.registers > 0)
-        assert(self.length >= 1)
-        assert(type(self.function_type) is FunctionType)
+        assert(type(self.function_type) is str)
+
+    ''' Rounded up length to be integer multiple of storage type '''
+    @property
+    def required_size(self):
+        return self.base_length + (GetDataTypeSize(self.dtype) * self.base_length) % GetDataTypeSize(self.storage_type)
 
     @property
-    def registers(self):
-        return self.storage_length
+    def storage_length(self):
+        return int(math.ceil(float(GetDataTypeSize(self.dtype) * self.required_size) / GetDataTypeSize(self.storage_type)))
 
+    @property
+    def dtype_ctype(self):
+        return GetDataTypeCType(self.dtype)
 
-
-class MapEntry(ModbusRegister):
-    def __init__(self, **kwargs):
-        if "register" in kwargs:
-            reg = kwargs["register"]
-            super().__init__(reg.name, reg.dtype, reg.length, reg.function_type)
-        else:
-            super().__init__(kwargs["name"], kwargs["dtype"], kwargs["length"], kwargs["function_type"])
-        self._address = None
 
     def set_address(self, address):
         self._address = address
@@ -52,26 +73,32 @@ class MapEntry(ModbusRegister):
         return self.address
 
     @property
+    def registers(self):
+        assert(self.storage_type =="uint16")
+        return self.storage_length
+
+    @property
     def dtype_ctype(self):
         return GetDataTypeCType(self.dtype)
 
     @property
     def read_code(self):
-        if self.function_type == FunctionType.kHolding:
+        if self.function_type == "holding":
             return 3
         return 4
 
     @property
     def write_code(self):
-        if self.function_type == FunctionType.kHolding:
+        if self.function_type == "holding":
             return 16
         else:
             assert(0)
 
     @property
     def memory_code(self):
-        return self.function_type.value
-
+        if self.function_type == "holding":
+            return 4
+        return 3
 
 class Slave:
     def __init__(self, address):
--- modbus_generator/data_store.py
+++ modbus_generator/data_store.py
@@ -1,9 +1,29 @@
 from jinja2 import Template, Environment, FileSystemLoader
 from os import path
 import datetime, os
-from data_stores import make, Struct, DataType
 
+class StructEntry:
+    def __init__(self, name, dtype, length, padding=0):
+        self.name = name
+        self.dtype = dtype
+        self.length = length
+        self.padding = padding
+
+        assert(type(self.name) is str)
+        assert(type(self.dtype) is str)
+        assert(self.length >= 1)
+
+    @property
+    def dtype_ctype(self):
+        return GetDataTypeCType(self.dtype)
 
 def MakeHeader(entries, fname, name):
-    make(Struct(name, entries, DataType.uint16_t), fname)
+    template_directory = path.join(path.dirname(__file__), 'templates')
+    env = Environment(loader=FileSystemLoader(template_directory))
+    register_map_template = "MappedDataStore.h.j2"
+    template = env.get_template(register_map_template)
+    rendering = template.render(name=name, entries=entries, timestamp=datetime.datetime.now())
+    with open(register_map_template.strip('.j2'), 'w') as f:
+        f.write(rendering)
+
     os.system("clang-format %s -i --style=Google"%fname)
